44629a734118b8a02f7b23a02d74a5e3b0e1d337
Никита <samnikita141204@yandex.ru>
2022-11-30 19:48:02 +0400

    Задача 32 максимальный локальный минимум

--- Program.cs
+++ Program.cs
@@ -64,7 +64,7 @@ WriteLine($" Сумма {number} элементов -> {summ} ");*/
 //Дан массив размером N.
 //Найти номер первого локального минимуму
 
-using static System.Console ;
+/*using static System.Console ;
 Clear ();
 
 int[] FillArrayRamdom(int size)
@@ -106,6 +106,60 @@ PritArray(arrayWork);
 int localMin=ChekLocalMin(arrayWork);
 if(localMin==-1)
     WriteLine(" Локального минимума нет "); 
-else WriteLine($" Номер позиции локального минимума {localMin}");
+else WriteLine($" Номер позиции локального минимума {localMin}");*/
 
 
+//Дан массив размером N.
+//Найти максимальный из его локальных минимумов
+
+using static System.Console ;
+Clear ();
+
+int[] FillArrayRamdom(int size)
+{
+    int[] array = new int[size];
+    for(int i=0; i<array.Length; i++)
+        array[i]=new Random().Next(1,11);
+    return array;
+}
+
+void PritArray(int[] array)
+{
+    for(int i=0; i<array.Length; i++)
+        Write(array[i] + " ");
+    WriteLine();
+}
+
+int ChekMaxLocalMin(int [] array)
+{
+    int localMin = -1;
+    if(array.Length<=1)
+        return localMin;
+
+    for(int i=1; i<array.Length-1; i++)
+    {
+        int left=array[i-1];
+        int middile=array[i];
+        int right=array[i+1];
+
+        if(left>middile && right>middile)
+        {
+            if(localMin < array[i])
+               localMin=array[i];    
+        }
+    } 
+    return localMin; 
+}
+
+
+Write("Введите размерность массива N -> ");
+int n=Convert.ToInt16(ReadLine());
+
+int[] arrayWork=FillArrayRamdom(n);
+PritArray(arrayWork);
+int localMinWork=ChekMaxLocalMin(arrayWork);
+
+if(localMinWork==-1)
+   WriteLine(" Локального минимума нет "); 
+
+ WriteLine($" Значение максимального локального минимума {localMinWork}");
