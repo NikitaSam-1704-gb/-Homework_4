a4221f6b56a4d3a4fbe23ca9386aec295d2156c7
Никита <samnikita141204@yandex.ru>
2022-11-30 16:25:17 +0400

    Задача суммы N элементов

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿// Напишите программу, которая принимает на вход трехзначное число 
 // на выходе показывает вторую цифру
 
-using static System.Console;
+/*using static System.Console;
 Clear() ;
 
 /*int ConsoleInput(string text )
@@ -22,9 +22,41 @@ int result=NumberSecondDigit(number);
 WriteLine($" число во втором разряде -> {result}");*/
 
 
-Write("Ввидите число N ");
+/*Write("Ввидите число N ");
 string N = ReadLine()!;
 if (N.Length > 2)
     WriteLine($"третье число {N[2].ToString()}");
 else 
-    WriteLine(" третьего знака нет ");
+    WriteLine(" третьего знака нет ");*/
+
+
+
+//Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+//7 -> 28
+//4 -> 10
+//8 -> 36
+
+using static System.Console ;
+ Clear () ;
+
+int ReadFromConsole(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt16(ReadLine());
+}
+
+int Summa(int numberTerms)
+{
+    int summa=0;
+    for(int i=1; i<=numberTerms; i++)
+    {
+        summa+=i ;
+    }
+    return summa;
+}
+
+string text=(" Введите число элементов ряда ");
+int number = ReadFromConsole(text);
+int summ=Summa(number);
+WriteLine($" Сумма {number} элементов -> {summ} ");
+
