66b38d0c60b44b65bd0eac5211a5d773901f32ec
Никита <samnikita141204@yandex.ru>
2022-11-30 17:41:35 +0400

    Задача 32, локальный мин массива, через методы

--- Program.cs
+++ Program.cs
@@ -36,7 +36,7 @@ else
 //4 -> 10
 //8 -> 36
 
-using static System.Console ;
+/*using static System.Console ;
  Clear () ;
 
 int ReadFromConsole(string message)
@@ -58,5 +58,54 @@ int Summa(int numberTerms)
 string text=(" Введите число элементов ряда ");
 int number = ReadFromConsole(text);
 int summ=Summa(number);
-WriteLine($" Сумма {number} элементов -> {summ} ");
+WriteLine($" Сумма {number} элементов -> {summ} ");*/
+
+
+//Дан массив размером N.
+//Найти номер первого локального минимуму
+
+using static System.Console ;
+Clear ();
+
+int[] FillArrayRamdom(int size)
+{
+    int[] array = new int[size];
+    for(int i=0; i<array.Length; i++)
+        array[i]=new Random().Next(1,11);
+    return array;
+}
+
+void PritArray(int[] array)
+{
+    for(int i=0; i<array.Length; i++)
+        Write(array[i] + " ");
+    WriteLine();
+}
+
+int ChekLocalMin(int [] array)
+{
+    if(array.Length<=1)
+        return -1;
+
+    for(int i=1; i<array.Length; i++)
+    {
+        int left=array[i-1];
+        int middile=array[i];
+        int right=array[i+1];
+        if(left>middile && right>middile)
+            return i;
+    } 
+    return -1;  
+}
+
+
+Write("Введите размерность массива N -> ");
+int n=Convert.ToInt16(ReadLine());
+int[] arrayWork=FillArrayRamdom(n);
+PritArray(arrayWork);
+int localMin=ChekLocalMin(arrayWork);
+if(localMin==-1)
+    WriteLine(" Локального минимума нет "); 
+else WriteLine($" Номер позиции локального минимума {localMin}");
+
 
