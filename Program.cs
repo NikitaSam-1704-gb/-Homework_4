d16f684151e40afb345588cb46f7716779c3d5ec
Никита <samnikita141204@yandex.ru>
2022-12-01 10:26:03 +0400

    Сохранил задачу 4

--- Program.cs
+++ Program.cs
@@ -318,7 +318,7 @@ int countWork=CountDigir(numberWork);
 
    WriteLine($" Количество цифр в числе -> {countWork} "); */
 
-//Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+/*//Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
 //4 -> 24 5 -> 120
 
 using static System.Console ;
@@ -344,4 +344,34 @@ double Factorial(int number)
 int numberWork=InputConsoleNumber("Введите натурально число N -> ");
 double summWork=Factorial(numberWork);
 
-WriteLine($" Факториал {numberWork} равен {summWork} ");
+WriteLine($" Факториал {numberWork} равен {summWork} ");*/
+
+//Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+//[1,0,1,1,0,1,0,0]
+
+using static System.Console;
+Clear();
+
+int [] FillArrayZerosOnes(int size)
+{
+    int[] array =new int[size];
+    for(int i=0; i<array.Length; i++)
+    {
+        array[i]=new Random().Next(0,2);
+    }
+    WriteLine();
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for(int i=0; i<array.Length; i++)
+    {
+        Write(array[i]+ " ");
+    }
+    WriteLine();
+}
+
+WriteLine( "Число элементов массива задано по умолчанию 8 ");
+int[] arrayWork=FillArrayZerosOnes(8);
+PrintArray(arrayWork);
